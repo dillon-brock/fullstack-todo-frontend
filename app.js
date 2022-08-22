6f24240be43f86843cf8f0a37ff0b01d5d9efdc9
Dillon Brock <103455520+dillon-brock@users.noreply.github.com>
2022-08-22 14:59:17 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+// import services and utilities
+
+// import component creators
+
+// declare state variables
+
+// write handler functions
+
+// Create each component: 
+// - pass in the root element via querySelector
+// - pass any needed handler functions as properties of an actions object 
+
+// Roll-up display function that renders (calls with state) each component
+function display() {
+    // Call each component passing in props that are the pieces of state this component needs
+}
+
+// Call display on page load
+display();
